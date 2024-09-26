8b1b8afe50defa5d2925a71a4fc21a6d8d85840e
sreekanth <imhsreekanth@gmail.com>
2024-09-26 17:55:04 +0530

    modified

--- index.js
+++ index.js
@@ -10,72 +10,10 @@ document.querySelectorAll('.menu li a').forEach(item => {
   });
   
   // Courses
-  document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll('.card');
   
-    // Trigger animation when the cards are visible on the screen
-    function revealCards() {
-      cards.forEach((card, index) => {
-        const cardPosition = card.getBoundingClientRect().top;
-        const screenPosition = window.innerHeight / 1.3;
-        if (cardPosition < screenPosition) {
-          card.style.setProperty('--delay', `${index * 0.2}s`);
-          card.classList.add('animated');
-        }
-      });
-    }
-  
-    // Run the revealCards function when the page is scrolled
-    window.addEventListener('scroll', revealCards);
-  
-    // Toggle card content when clicked
-    cards.forEach(card => {
-      card.addEventListener('click', function () {
-        const content = this.querySelector('.card-content');
-        if (content.style.display === 'block') {
-          content.style.display = 'none';
-        } else {
-          content.style.display = 'block';
-        }
-      });
-    });
-  });
   
   // New Batches
-  document.addEventListener("DOMContentLoaded", function () {
-    const batchCards = document.querySelectorAll('.batch-card');
-  
-    function revealCards(cards) {
-      cards.forEach((card, index) => {
-        const cardPosition = card.getBoundingClientRect().top;
-        const screenPosition = window.innerHeight / 1.3;
-        if (cardPosition < screenPosition) {
-          card.style.setProperty('--delay', `${index * 0.2}s`);
-          card.classList.add('animated');
-        }
-      });
-    }
-  
-    function toggleCardContent(cards) {
-      cards.forEach(card => {
-        card.addEventListener('click', function () {
-          const content = this.querySelector('.batch-content');
-          content.style.display = content.style.display === 'block' ? 'none' : 'block';
-        });
-      });
-    }
-  
-    // Reveal cards on scroll
-    window.addEventListener('scroll', () => {
-      revealCards(batchCards);
-    });
-  
-    // Toggle card content on click
-    toggleCardContent(batchCards);
-  
-    // Initial reveal of cards on page load
-    revealCards(batchCards);
-  });
+ 
   
   // Job Openings
   document.addEventListener("DOMContentLoaded", function () {
